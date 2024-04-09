a980875d280f3eb5d663261e472f92d6f65e001a
vishnu360core <161002570+vishnu360core@users.noreply.github.com>
2024-04-09 18:33:11 +0530

    Added the horse models

---
+++ Horse_RacingGame/Assets/Scripts/Layout.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Layout : MonoBehaviour
+{
+    public enum layoutType { horizontal,vertical}
+    public enum layoutDirection {left,right,up,down}
+
+
+    [Header("LayoutType:")]
+    public layoutType _type;
+
+   
+    [Header("Direction:")]
+    public layoutDirection _direction;  
+
+    [Header("Reference Object:")]
+    [SerializeField] GameObject refObject;
+
+    [Space]
+    [Range(0, 10.0f)]
+    [SerializeField] float _gap;
+
+   
+    [Header("list:")]
+    [SerializeField] List<GameObject> objects = new List<GameObject>();
+
+    private void Awake()
+    {
+        float zlength = refObject.transform.localScale.z;
+        float xlength = refObject.transform.localScale.x + _gap;
+
+        switch (_type) 
+        {
+             case layoutType.horizontal:
+
+                    for (int i = 1;i<objects.Count;++i)
+                    {
+                        if(_direction == layoutDirection.right)
+                          objects[i].transform.position = new Vector3(refObject.transform.position.x - i * xlength, refObject.transform.position.y,refObject.transform.position.z);
+                        else
+                          objects[i].transform.position = new Vector3(refObject.transform.position.x + i * xlength, refObject.transform.position.y, refObject.transform.position.z);
+                    }
+
+                    break;
+
+
+                case layoutType.vertical:
+                break;
+
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
