fc8549c61ddaba7221c29bae6206f7bd805e5c5e
vishnu360core <161002570+vishnu360core@users.noreply.github.com>
2024-04-13 13:30:18 +0530

    Added the spline animation with horse and camera target

--- Horse_RacingGame/Assets/Scripts/Horse.cs
+++ Horse_RacingGame/Assets/Scripts/Horse.cs
@@ -1,18 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Splines;
 
+[RequireComponent(typeof(SplineAnimate))]
+[RequireComponent (typeof(Animator))]
 public class Horse : MonoBehaviour
 {
+    SplineAnimate splineAnimate;
+    Animator animator;
+
+    Vector3 _currentPos = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        splineAnimate = this.GetComponent<SplineAnimate>();
+        animator = this.GetComponent<Animator>();
+
+        splineAnimate.onUpdated += OnSplineUpdate;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnSplineUpdate(Vector3 vector, Quaternion quaternion)
     {
-        
+       if((int)splineAnimate.ElapsedTime == (int)splineAnimate.duration)
+       {
+            Debug.Log("Stopped");
+
+            animator.Play("Idle");
+            return;
+       }
+    }
+
+  
+    /// <summary>
+    /// Reset the horse
+    /// </summary>
+    void ResetAction()
+    {
+        animator.Play("Run");
     }
 }
